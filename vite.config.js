58cf745bcf82f304d992431dbd18ff417aeef3f1
UtsavCK <82099161+UtsavCK@users.noreply.github.com>
2024-12-11 22:22:04 +0545

    added vite. emailjs works on local device

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,14 @@
 import { defineConfig } from 'vite';
 import dotenv from 'dotenv';
 
-// Load environment variables from .env file
 dotenv.config();
 
 export default defineConfig({
     define: {
         'process.env': {
-            EMAILJS_PUBLIC_KEY: process.env.EMAILJS_PUBLIC_KEY,
-            EMAILJS_SERVICE_ID: process.env.EMAILJS_SERVICE_ID,
-            EMAILJS_TEMPLATE_ID: process.env.EMAILJS_TEMPLATE_ID,
+            EMAILJS_PUBLIC_KEY: process.env.VITE_EMAILJS_PUBLIC_KEY,
+            EMAILJS_SERVICE_ID: process.env.VITE_EMAILJS_SERVICE_ID,
+            EMAILJS_TEMPLATE_ID: process.env.VITE_EMAILJS_TEMPLATE_ID,
         },
     },
 });
